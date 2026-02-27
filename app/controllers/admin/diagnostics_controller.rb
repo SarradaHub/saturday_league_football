a05e21543f5b9c236064bf8fe4490de922620310
LMafra <chagas.lucas.mafra@gmail.com>
2026-02-27 11:03:50 -0300

    fix migration diagnostics

--- app/controllers/admin/diagnostics_controller.rb
+++ app/controllers/admin/diagnostics_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DiagnosticsController < ApplicationController
   def migrations
-    context = ActiveRecord::Base.connection.migration_context
+    context  = ActiveRecord::MigrationContext.new(ActiveRecord::Migrator.migrations_paths)
     statuses = context.migrations_status
 
     pending = statuses.select { |state, _version, _name| state == 'down' }
@@ -16,3 +16,4 @@ class Admin::DiagnosticsController < ApplicationController
     render json: { error: e.message }, status: :internal_server_error
   end
 end
+
